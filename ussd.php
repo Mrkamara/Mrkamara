c435f6923525000626848ce6161ba2829f182250
Mrkamara <82453535+Mrkamara@users.noreply.github.com>
2021-04-13 12:58:11 +0000

    Update ussd.php

--- ussd.php
+++ ussd.php
@@ -92,11 +92,3 @@ $response = "END Your Table reservation for 8 has been canceled";
 header('Content-type: text/plain');
 echo $response
 ?>
-From the above code snippet, we have some variables and keywords:
-$sessionId: This generates a unique value when the session starts and sent every time a mobile subscriber response has been received.
-$serviceCode: This refer to your USSD code
-$text: This shows the user input. which is an empty string in the first notification of a session which after that concatenates all the user input within the session until the session ends.
-$response: This hold the answer to the user input.
-echo: Prints out the response for the user to read.
-CON: It means an intermediate menu Or that the session is CONtinuing
-END: Means the final menu and will trigger session termination i.e session is ENDing.
